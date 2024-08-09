b28c53e0e8f3b1ebcbc57746907510aeb8344974
Edson Sensato <esensato@hotmail.com>
2024-08-08 21:22:05 -0300

    08/08/2024

--- config-mac.sh
+++ config-mac.sh
@@ -2,5 +2,5 @@ echo "Configurando gradle"
 echo "Local de instalacao do gradle: $1"
 echo "export PATH=$PATH:$1" >> ~/.zshrc
 echo "export PATH=$PATH:$1" >> ~/.bash_profile
-echo "OK! einicie o VS Code..."
+echo "OK! Reinicie o VS Code..."
 
